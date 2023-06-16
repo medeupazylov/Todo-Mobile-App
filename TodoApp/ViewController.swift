c325841e18f735d460f4974cb6fbbdd5ab1ba67f
medeupazylov <113336831+medeupazylov@users.noreply.github.com>
2023-06-17 01:27:01 +0600

    first commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Medeu Pazylov on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
